272c465f49d62c6f95b5303b88b623cb919d2b09
cwauge <32182433+cwauge@users.noreply.github.com>
2023-11-26 15:12:18 -1000

    set up class

--- read_vis_class.py
+++ read_vis_class.py
@@ -1,6 +1,17 @@
+'''
+Connor Auge - Created Nov. 26, 2023
+Class to read in visual morphology classifications from .csv file (fromatted like excel/google sheets)
+'''
+
 import numpy as np
 from astropy.io import fits
 from astropy.io import ascii
 
-import matplotlib.pyplot as plt
-# Check git hub add
+def main(fname):
+    f = Read_File(fname)
+
+class Read_File():
+    
+    def __init__(self,fname):
+        self.fname = fname
+
