5bbfe773241df2c4e770a0d01a7224f9ae8c30a4
cwauge <32182433+cwauge@users.noreply.github.com>
2023-12-12 16:23:17 -1000

    Run for full sample

--- RunClassifications.py
+++ RunClassifications.py
@@ -16,14 +16,14 @@ with fits.open('/Users/connor_auge/Research/Disertation/catalogs/AHA_SEDs_out_AL
     sed_data = hdul[1].data 
 
 sed_field = sed_data['field']
-sed_id = sed_data['id'][sed_field == 'COSMOS']
-sed_z = sed_data['z'][sed_field == 'COSMOS']
-sed_x = sed_data['x'][sed_field == 'COSMOS']
-sed_y = sed_data['y'][sed_field == 'COSMOS']
-sed_Lx = sed_data['Lx'][sed_field == 'COSMOS']
-sed_norm = sed_data['norm'][sed_field == 'COSMOS']
-sed_shape = sed_data['shape'][sed_field == 'COSMOS']
-check_sed = sed_data['check6'][sed_field == 'COSMOS']
+sed_id = sed_data['id']#[sed_field == 'COSMOS']
+sed_z = sed_data['z']#[sed_field == 'COSMOS']
+sed_x = sed_data['x']#[sed_field == 'COSMOS']
+sed_y = sed_data['y']#[sed_field == 'COSMOS']
+sed_Lx = sed_data['Lx']#[sed_field == 'COSMOS']
+sed_norm = sed_data['norm']#[sed_field == 'COSMOS']
+sed_shape = sed_data['shape']#[sed_field == 'COSMOS']
+check_sed = sed_data['check6']#[sed_field == 'COSMOS']
 
 sed_id = sed_id[check_sed == 'GOOD']
 sed_z = sed_z[check_sed == 'GOOD'] 
@@ -34,12 +34,14 @@ sed_norm = sed_norm[check_sed == 'GOOD']
 sed_shape = sed_shape[check_sed == 'GOOD']
 check_sed = check_sed[check_sed == 'GOOD']
 
-inf = Read_File('Auge_COSMOS_Classifications_read.xlsx')
+# inf = Read_File('Auge_COSMOS_Classifications_read.xlsx')
+inf = Read_File('Auge_Classifications_read_total.xlsx')
 inf.open(type='xlsx')
 
 cols = inf.columns() # Columns of file. Name of classification
 data = inf.data()    # Data of file. Array of nans and Xs. No IDs or notes
 morph_ID = inf.IDs()       # ID column of file
+morph_field = inf.field()
 
 inf.x_to_one(data)   # Turn the Xs in the data array to 1s
 
@@ -59,16 +61,19 @@ print('PS_flag:       ',len(dict_out['PS_flag']),np.nansum(dict_out['PS_flag']))
 
 
 plot = Plotter(cols,dict_out)
-plot_shape = Shape_Plotter(cols,dict_out,morph_ID,sed_id,sed_shape)
+plot_shape = Shape_Plotter(cols,dict_out,morph_ID,sed_id,sed_shape,sed_field,morph_field)
 # cosmos_disp = Display(cols,dict_out,morph_ID,sed_id,sed_shape,sed_x,sed_y,sed_z,sed_Lx,'/Users/connor_auge/Research/Disertation/morphology/visual/COSMOS/cosmos_cutouts_sample_published/')
 # main(sed_id,cols,dict_out,morph_ID,sed_id,sed_shape,sed_x,sed_y,sed_z,sed_Lx,'/Users/connor_auge/Research/Disertation/morphology/visual/COSMOS/cosmos_cutouts_sample_published/')
 
-# plot_shape.shape_class_bar('_new/bar_shape',flag='tf',bins='shape',save=True)
-# plot_shape.shape_class_bar('_new/bar_shape_frac',flag='tf',bins='shape',fractional='bin',save=True)
-# plot_shape.shape_class_bar('_new/bar_shape_frac_tot',flag='tf',bins='shape',fractional='total',save=True)
+plot_shape.shape_class_bar('_new/total_bar_shape_tf',flag='tf',bins='shape',save=True)
+# plot_shape.shape_class_bar('_new/total_bar_shape_ps',flag='ps',bins='shape',save=True)
+# plot_shape.shape_class_bar('_new/total_bar_shape_merger',flag='merger',bins='shape',save=True)
+
+# plot_shape.shape_class_bar('_new/total_bar_shape_frac',flag='tf',bins='shape',fractional='bin',save=True)
+# plot_shape.shape_class_bar('_new/total_bar_shape_frac_tot',flag='tf',bins='shape',fractional='total',save=True)
 
 
 plot.bar('_new/total_bar_tf',flag='tf',save=True)
-plot.bar('_new/total_bar_ps',flag='PS',save=True)
-plot.bar('_new/total_bar_merg',flag='merger',save=True)
+# plot.bar('_new/total_bar_ps',flag='PS',save=True)
+# plot.bar('_new/total_bar_merg',flag='merger',save=True)
 
